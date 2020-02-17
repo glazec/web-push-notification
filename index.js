a14bd8b2d0619eb1e5683699446a40fa9e09a2ca
glaze <luyiping1011@gmail.com>
2020-02-17 14:11:10 -0800

    index.js:add more coment

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const github = require("@actions/github");
 async function main() {
   try {
     // Get newPost.json from your site.
-    var newPostOnlineSite = await fetch(
-      "https://www.inevitable.tech/newPost.json"
-    );
+    var newPostOnlineSite = await fetch(process.env.newPostOnlineSite);
     newPostOnlineSite = await newPostOnlineSite.json();
     newPostOnlineSite = JSON.parse(JSON.stringify(newPostOnlineSite));
     // Get newPost.json from your repo.
-    var newPostRepo = await fetch(
-      "https://raw.githubusercontent.com/glazec/glazec.github.io/master/newPost.json"
-    );
+    var newPostRepo = await fetch(process.env.newPostRepo);
+    // Get newPost.json from the repo
     newPostRepo = await newPostRepo.json();
     newPostRepo = JSON.parse(JSON.stringify(newPostRepo));
-    console.table({"From online site":newPostOnlineSite,"From Repo":newPostRepo})
-    // console.info(newPostOnlineSite);
-    // console.info(newPostRepo);
+    console.table({
+      "From online site": newPostOnlineSite,
+      "From Repo": newPostRepo
+    });
 
     //publish the site
     fetch(process.env.buildHook, { method: "POST" });
+    console.info("Trigger Netlify deploy");
 
+    //determine whether to push web notification
     if (newPostOnlineSite.id != newPostRepo.id) {
       // push new Post notification
 
@@ -47,13 +47,13 @@ async function main() {
       if (!response.ok) {
         // NOT res.status >= 200 && res.status < 300
         console.info(JSON.stringify(data));
-        // core.setOutput(
-        //   "msg",
-        //   JSON.stringify({ statusCode: data.status, body: data.detail })
-        // );
+        core.setOutput(
+          "msg",
+          JSON.stringify({ statusCode: data.status, body: data.detail })
+        );
       } else {
         console.info("Successfully push notification");
-        // core.setOutput("msg", "Successfully push notification");
+        core.setOutput("msg", "Successfully push notification");
       }
     } else {
       console.info("No New Post detected.");
