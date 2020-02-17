27b1d07e9cb9d93637e291c977f350b481c3a45e
glaze <luyiping1011@gmail.com>
2020-02-17 13:33:02 -0800

    format

--- index.js
+++ index.js
@@ -1,56 +1,66 @@
-const fetch = require('node-fetch');
-const core = require('@actions/core');
-const github = require('@actions/github');
+const fetch = require("node-fetch");
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function main() {
-    try {
-        // Get newPost.json from your site.
-        var newPostOnlineSite = await fetch('https://www.inevitable.tech/newPost.json')
-        newPostOnlineSite = await newPostOnlineSite.json();
-        newPostOnlineSite = JSON.parse(JSON.stringify(newPostOnlineSite));
-        // Get newPost.json from your repo.
-        var newPostRepo = await fetch('https://raw.githubusercontent.com/glazec/glazec.github.io/master/newPost.json')
-        newPostRepo = await newPostRepo.json();
-        newPostRepo = JSON.parse(JSON.stringify(newPostRepo))
-        console.log(newPostOnlineSite)
-        console.log(newPostRepo)
+  try {
+    // Get newPost.json from your site.
+    var newPostOnlineSite = await fetch(
+      "https://www.inevitable.tech/newPost.json"
+    );
+    newPostOnlineSite = await newPostOnlineSite.json();
+    newPostOnlineSite = JSON.parse(JSON.stringify(newPostOnlineSite));
+    // Get newPost.json from your repo.
+    var newPostRepo = await fetch(
+      "https://raw.githubusercontent.com/glazec/glazec.github.io/master/newPost.json"
+    );
+    newPostRepo = await newPostRepo.json();
+    newPostRepo = JSON.parse(JSON.stringify(newPostRepo));
+    console.log(newPostOnlineSite);
+    console.log(newPostRepo);
 
-        //publish the site
-        fetch(process.env.buildHook, { method: 'POST' });
+    //publish the site
+    fetch(process.env.buildHook, { method: "POST" });
 
-        if (newPostOnlineSite.id != newPostRepo.id) {
-            // push new Post notificaiton
+    if (newPostOnlineSite.id != newPostRepo.id) {
+      // push new Post notificaiton
 
-            payload = { "title": newPostRepo.title, "message": newPostRepo.summary, "target_url": newPostRepo.url }
-            const response = await fetch('https://app.webpushr.com/api/v1/notification/send/all', {
-                method: 'POST',
-                headers: {
-                    'webpushrKey': process.env.webpushrKey,
-                    'webpushrAuthToken': process.env.webpushrAuthToken,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(payload),
-            });
-            const data = await response.json();
-            if (!response.ok) {
-                // NOT res.status >= 200 && res.status < 300 
-                console.log(JSON.stringify(data))
-                core.setOutput("msg", JSON.stringify({ statusCode: data.status, body: data.detail }));
-            }
-            else {
-                console.log("Successfully push notification")
-                core.setOutput("msg", "Successfully push notification")
-            }
+      payload = {
+        title: newPostRepo.title,
+        message: newPostRepo.summary,
+        target_url: newPostRepo.url
+      };
+      const response = await fetch(
+        "https://app.webpushr.com/api/v1/notification/send/all",
+        {
+          method: "POST",
+          headers: {
+            webpushrKey: process.env.webpushrKey,
+            webpushrAuthToken: process.env.webpushrAuthToken,
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(payload)
         }
-        else {
-            console.log("No New Post detected.")
-            core.setOutput("msg", "No New Post detected.");
-        }
-
-    } catch (error) {
-        core.setFailed(error.message);
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        // NOT res.status >= 200 && res.status < 300
+        console.log(JSON.stringify(data));
+        core.setOutput(
+          "msg",
+          JSON.stringify({ statusCode: data.status, body: data.detail })
+        );
+      } else {
+        console.log("Successfully push notification");
+        core.setOutput("msg", "Successfully push notification");
+      }
+    } else {
+      console.log("No New Post detected.");
+      core.setOutput("msg", "No New Post detected.");
     }
-
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-main()
+main();
